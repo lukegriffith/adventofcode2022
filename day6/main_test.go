03a759345665b7cf92552301424ea58194e7858f
lukegriffith <lukemgriffith@gmail.com>
2022-12-06 11:09:14 +0000

    Adding work on day6

--- day6/main_test.go
+++ day6/main_test.go
@@ -40,11 +40,3 @@ func TestDecodeMessage(t *testing.T) {
 		}
 	}
 }
-
-/*
-mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
-bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
-nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
-nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
-zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
-*/
